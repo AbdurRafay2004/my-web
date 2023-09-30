fa56b28b71623d29699e2201de09c0f2139afd5d
Abdur Rafay <rafay.mail2004@gmail.com>
2023-09-30 18:07:42 +0600

    introduction changed

--- Introduction.py
+++ Introduction.py
@@ -24,27 +24,30 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+   
 
+    st.write("# Don't mind me, I'm just testing things out 👋")
+
+    
     st.sidebar.success("Select a demo above.")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
+        I will try to create a CGPA calculator using **Python** and **Streamlit**
+
+        **👈 Select my demos from the sidebar** 
+
+        ### testing bullet list
+        - 1
+        - 2
+        - 3
+      
+        
+        """
+    
     )
+
+    st.write("[~rafay](https://www.facebook.com/AbdurRafay2004/)")
 
 
 if __name__ == "__main__":
